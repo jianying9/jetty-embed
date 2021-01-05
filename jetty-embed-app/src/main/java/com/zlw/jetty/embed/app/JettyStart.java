75aa51159aefda88d583f2bef84cce9aeffc8d09
jianying9 <18659199765@163.com>
2021-01-05 14:42:32 +0800

    jvm启动参数优化

--- jetty-embed-app/src/main/java/com/zlw/jetty/embed/app/JettyStart.java
+++ jetty-embed-app/src/main/java/com/zlw/jetty/embed/app/JettyStart.java
@@ -12,6 +12,7 @@ public class JettyStart
 
     public static void main(String[] args)
     {
+        //nohup java -jar -server -XX:MetaspaceSize=32m -XX:MaxMetaspaceSize=32m -Xms128m -Xmx128m -XX:+UseG1GC -XX:ParallelGCThreads=2 jetty-embed-app.jar >/dev/null 2>&1 &
         int port = 8080;
         String appName = "base";
         ServerBuilder serverBuilder = new ServerBuilder(appName, port);
